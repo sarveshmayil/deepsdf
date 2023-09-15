cab9a5754c5b7373e567bea88d8ee0960ca64669
Sarvesh Mayilvahanan <sarvesh.mayil@gmail.com>
2023-09-15 14:55:47 -0400

    fished training script

--- train_deep_sdf.py
+++ train_deep_sdf.py
@@ -1,11 +1,14 @@
 import yaml
+import json
 import argparse
 import numpy as np
 import torch
+from torch.utils.data import DataLoader
 import os
 from tqdm import tqdm
 
 from network.deep_sdf_network import Decoder
+from utils.sampling import SDF_Dataset
 
 
 def get_args():
@@ -20,7 +23,7 @@ def get_args():
     parser.add_argument(
         "--output", "-o",
         dest="save_dir",
-        required=True,
+        default=os.path.join(os.path.dirname(__file__), "results", "checkpoints"),
         help="Path to directory where checkpoints are saved"
     )
 
@@ -35,9 +38,18 @@ def main(args, save_dir):
     network_kwargs = args['network_specs']
     n_epochs = args['epochs']
 
+    os.makedirs(save_dir, exist_ok=True)
+
+    with open(args['train_test_split'], 'r') as file:
+        training_paths = json.load(file)['train']
+        training_paths = [os.path.join(args['data_dir'], path) for path in training_paths]
+
+    dataset = SDF_Dataset(training_paths, device)
+    data_loader = DataLoader(dataset, batch_size=args['batch_size'], shuffle=True)
+
     decoder = Decoder(latent_dim, **network_kwargs).to(device)
 
-    latent_vecs = torch.nn.Embedding(n_objects, latent_dim, max_norm=args['latent_vec_bound'])
+    latent_vecs = torch.nn.Embedding(len(dataset), latent_dim, max_norm=args['latent_vec_bound'], device=device)
     torch.nn.init.normal_(latent_vecs, mean=0.0, std=1.0 / np.sqrt(latent_dim))
 
     l1_loss = torch.nn.L1Loss(reduction="sum")
@@ -58,12 +70,41 @@ def main(args, save_dir):
     decoder.train()
     pbar = tqdm(total=n_epochs, desc="Deep SDF Training")
     for epoch in range(n_epochs):
+        # Updates learning rates according to specified 
         optimizer.param_groups[0]['lr'] = args['network_lr_schedule']['start'] * args['network_lr_schedule']['decay'] ** (epoch // args['network_lr_schedule']['interval'])
         optimizer.param_groups[1]['lr'] = args['latent_vec_lr_schedule']['start'] * args['latent_vec_lr_schedule']['decay'] ** (epoch // args['latent_vec_lr_schedule']['interval'])
 
         # Get data from dataloader
         # preprocess data
         # split into batches, pass through model, evaluate loss, backprop
+
+        for data, obj_idxs in data_loader:
+            data.requires_grad = False
+            points = data[:,:3]  # get [x y z] coordinates
+            sdf_true = torch.clamp(data[:,-1], -args['sdf_clamping_dist'], args['sdf_clamping_dist'])  # get ground truth SDF value and clamp between provided values
+            n_samples = data.shape[0]
+
+            # chunk data
+            points = torch.chunk(points, args['batch_split'])  # (batch_split, n, 3)
+            sdf_true = torch.chunk(sdf_true, args['batch_split'])  # (batch_split, n, 1)
+            obj_idxs = torch.chunk(obj_idxs, args['batch_split'])  # (batch_split, n, 1)
+
+            batch_loss = 0.0
+            optimizer.zero_grad()
+            for i in range(args['batch_split']):
+                batch_latent_vecs = latent_vecs(obj_idxs[i])
+                inp = torch.cat((batch_latent_vecs, points[i]), dim=1)  # stack latent vectors and 3D coordinates
+
+                sdf_pred = decoder(inp)
+                sdf_pred = torch.clamp(sdf_pred, -args['sdf_clamping_dist'], args['sdf_clamping_dist'])  # clamp predictions to match GT
+                batch_split_loss = l1_loss(sdf_pred, sdf_true[i]) / n_samples
+
+                if args['latent_vec_regularization']:
+                    latent_vec_reg_loss = args['latent_vec_reg_lambda'] * min(1, epoch/100) * torch.sum(torch.norm(batch_latent_vecs, p=2, dim=1)) / n_samples
+                    batch_split_loss += latent_vec_reg_loss
+
+                batch_loss += batch_split_loss
+                batch_split_loss.backward()
 
         optimizer.step()
 
@@ -76,6 +117,6 @@ def main(args, save_dir):
 if __name__ == "__main__":
     args = get_args()
     with open(args.config, 'r') as file:
-        config_args = yaml.load(file)
+        config_args = yaml.safe_load(file)
 
     main(config_args, args.save_dir)
