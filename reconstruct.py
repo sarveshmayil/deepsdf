5c70d1af79f580b0bf90fc1b1e6355034d05a3d5
Sarvesh Mayilvahanan <sarvesh.mayil@gmail.com>
2023-09-17 20:23:02 -0400

    full reconstruction for test set [doesn't work]

--- reconstruct.py
+++ reconstruct.py
@@ -1,14 +1,13 @@
 import yaml
 import json
 import argparse
-import numpy as np
-import matplotlib.pyplot as plt
 import torch
-from torch.utils.data import DataLoader
+import trimesh
 import os
 from tqdm import tqdm
 
 from network.deep_sdf_network import Decoder
+from utils.mesh import create_mesh
 from utils.sampling import SDF_Dataset
 
 
@@ -36,28 +35,18 @@ def get_args():
     )
 
     parser.add_argument(
-        "--iters",
-        dest="iterations",
-        default=800,
-        help="The number of iterations of latent code optimization to perform."
-    )
-
-    parser.add_argument(
-        "--mesh", "-m",
-        dest="create_mesh",
+        "--visualize", "-v",
+        dest="visualize",
         action="store_true",
-        help="Flag for creating mesh of objects"
+        help="Flag for visualization of reconstructed mesh"
     )
 
     args = parser.parse_args()
     return args
 
-def save_model(directory, filename, model, epoch=None):
-    if epoch is not None:
-        save_path = os.path.join(directory, filename+"%d.pth" % (epoch))
-    else:
-        save_path = os.path.join(directory, filename+".pth")
-    torch.save(model.state_dict(), save_path)
+def save_latent_vec(directory, filename, latent_vec):
+    save_path = os.path.join(directory, filename+".pth")
+    torch.save(latent_vec, save_path)
 
 def main(trained_model_path, args, save_dir, visualize):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
@@ -72,59 +61,55 @@ def main(trained_model_path, args, save_dir, visualize):
         test_paths = json.load(file)['test']
         test_paths = [os.path.join(args['data_dir'], path) for path in test_paths]
 
+    dataset = SDF_Dataset(test_paths, device)
+
     decoder = Decoder(latent_dim, **network_kwargs)
-    trained_decoder_state = torch.load(trained_model_path)
-    decoder.load_state_dict(trained_decoder_state['model_state_dict'])
+    decoder.load_state_dict(torch.load(trained_model_path))
     decoder = decoder.to(device)
     decoder.eval()
 
-    l1_loss = torch.nn.L1Loss(reduction="sum")
+    l1_loss = torch.nn.L1Loss()
 
-    for test_path in test_paths:
+    for i, test_path in enumerate(test_paths):
         latent_vec = torch.normal(mean=torch.zeros((1, latent_dim)), std=0.01).to(device)
         latent_vec.requires_grad = True
-
-        dataset = SDF_Dataset([test_path], device)
-        data_loader = DataLoader(dataset, batch_size=args['batch_size'], shuffle=True)
 
         optimizer = torch.optim.Adam(
             [latent_vec],
             lr=args['reconstruct_lr_schedule']['start']
         )
 
+        data, _ = dataset[i]
+        data.requires_grad = False
+        points = data[:,:3]  # get [x y z] coordinates
+        sdf_true = torch.clamp(data[:,-1], -args['sdf_clamping_dist'], args['sdf_clamping_dist']).unsqueeze(1)  # get ground truth SDF value and clamp between provided values
+        print(sdf_true.min(), sdf_true.max())
+
         for epoch in tqdm(range(n_epochs)):
             optimizer.param_groups[0]['lr'] = args['reconstruct_lr_schedule']['start'] * args['reconstruct_lr_schedule']['decay'] ** (epoch // args['reconstruct_lr_schedule']['interval'])
 
-            for data, _ in data_loader:
-                data.requires_grad = False
-                data = data.reshape(-1, 4)  # stack inputs into 2D
-                points = data[:,:3]  # get [x y z] coordinates
-                sdf_true = torch.clamp(data[:,-1], -args['sdf_clamping_dist'], args['sdf_clamping_dist']).unsqueeze(1)  # get ground truth SDF value and clamp between provided values
-                n_samples = data.shape[0]
+            optimizer.zero_grad()
+            inp = torch.cat((latent_vec.expand(points.shape[0], -1), points), dim=1)  # stack latent vectors and 3D coordinates
 
-                # chunk data
-                points = torch.chunk(points, args['batch_split'])  # (batch_split, n, 3)
-                sdf_true = torch.chunk(sdf_true, args['batch_split'])  # (batch_split, n, 1)
+            sdf_pred = decoder(inp)
+            sdf_pred = torch.clamp(sdf_pred, -args['sdf_clamping_dist'], args['sdf_clamping_dist'])  # clamp predictions to match GT
+            loss = l1_loss(sdf_pred, sdf_true)
 
-                batch_loss = 0.0
-                optimizer.zero_grad()
-                for i in range(args['batch_split']):
-                    inp = torch.cat((latent_vec.expand(points[i].shape[0], 1), points[i]), dim=1)  # stack latent vectors and 3D coordinates
+            if args['latent_vec_regularization']:
+                latent_vec_reg_loss = args['latent_vec_reg_lambda'] * torch.mean(torch.pow(latent_vec, 2))
+                loss += latent_vec_reg_loss
 
-                    sdf_pred = decoder(inp)
-                    sdf_pred = torch.clamp(sdf_pred, -args['sdf_clamping_dist'], args['sdf_clamping_dist'])  # clamp predictions to match GT
-                    batch_split_loss = l1_loss(sdf_pred, sdf_true[i]) / n_samples
+            loss.backward()
+            optimizer.step()
 
-                    if args['latent_vec_regularization']:
-                        latent_vec_reg_loss = args['latent_vec_reg_lambda'] * min(1, epoch/100) * torch.sum(torch.norm(latent_vec, p=2)) / n_samples
-                        batch_split_loss += latent_vec_reg_loss
+        save_latent_vec(os.path.join(save_dir, "latent_vecs"), os.path.splitext(os.path.basename(test_path))[0], latent_vec)
 
-                    batch_loss += batch_split_loss
-                    batch_split_loss.backward()
-
-                optimizer.step()
-
-        save_model(os.path.join(save_dir, "latent_vecs"), os.path.splitext(os.path.basename(test_path))[0], latent_vec)
+        if visualize:
+            vertices, faces = create_mesh(decoder, latent_vec, N=128)
+            scene = trimesh.Scene()
+            reconstruct_mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+            scene.add_geometry(reconstruct_mesh)
+            scene.show()
 
 if __name__ == "__main__":
     args = get_args()
---
+++ utils/mesh.py
@@ -0,0 +1,44 @@
+import torch
+from skimage.measure import marching_cubes
+from tqdm import tqdm
+
+
+def create_mesh(decoder, latent_vec, N:int=256, batch_size:int=32768):
+    device = latent_vec.device
+    decoder.eval()
+
+    # Marching Cubes Algorithm
+    voxel_origin = torch.LongTensor([-1, -1, -1])
+    voxel_size = 2 / (N - 1)
+
+    n_samples = N**3
+
+    overall_index = torch.arange(0, n_samples, out=torch.LongTensor())
+
+    samples = torch.zeros(n_samples, 4)
+    samples[:,0] = ((overall_index // N) // N) % N
+    samples[:,1] = (overall_index // N) % N
+    samples[:,2] = overall_index % N
+
+    samples[:,:3] = samples[:,:3] * voxel_size + voxel_origin
+    samples.requires_grad = False
+
+    head = 0
+    pbar = tqdm(total=n_samples, desc="Computing SDF grid")
+    while head < n_samples:
+        sample_subset = samples[head:min(head+batch_size, n_samples), :3]
+        inp = torch.cat((latent_vec.expand(sample_subset.shape[0], -1), sample_subset.to(device)), dim=1)  # stack latent vectors and 3D coordinates
+        samples[head:min(head+batch_size, n_samples), 3] = decoder(inp).squeeze(1).detach().cpu()
+        head += batch_size
+        pbar.update(batch_size)
+    pbar.close()
+
+    sdf_pred = samples[:,3].reshape(N,N,N)
+    sdf_grid = sdf_pred.cpu().numpy()
+    print(sdf_grid.min(), sdf_grid.max())
+    print("created grid")
+    vertices, faces, _, _ = marching_cubes(sdf_grid, level=0.0, spacing=[voxel_size, voxel_size, voxel_size])
+    print("cubes marched")
+
+    return vertices, faces
+        
