d14230b0b1adf7f80c183ea314145469c469f07f
Sarvesh Mayilvahanan <sarvesh.mayil@gmail.com>
2023-09-17 20:22:08 -0400

    added option to subsample dataset

--- utils/sampling.py
+++ utils/sampling.py
@@ -8,7 +8,7 @@ import torch
 from typing import Tuple, List
 
 class MeshSampler:
-    def __init__(self, mesh:trimesh.Trimesh, n_points:int=500, dist_stdv:float=0.0005**0.5) -> None:
+    def __init__(self, mesh:trimesh.Trimesh, n_points:int=500000, dist_stdv:float=0.0005**0.5) -> None:
         self.mesh = mesh
         self.n_points = n_points
         self.dist_stdv = dist_stdv
@@ -28,8 +28,9 @@ class MeshSampler:
     
 
 class SDF_Dataset(torch.utils.data.Dataset):
-    def __init__(self, sdf_paths:List[str], device:str='cpu'):
+    def __init__(self, sdf_paths:List[str], n_points:int=None, device:str='cpu'):
         self.sdf_paths = sdf_paths
+        self.n_points = n_points
         self.device = device
 
     def __len__(self):
@@ -46,6 +47,9 @@ class SDF_Dataset(torch.utils.data.Dataset):
         """
         path = self.sdf_paths[idx]
         point_sdf_pairs = self.load_samples(path)
+        if self.n_points is not None:
+            assert self.n_points <= point_sdf_pairs.shape[0], "Can't select more than available number of samples"
+            point_sdf_pairs = point_sdf_pairs[np.random.choice(point_sdf_pairs.shape[0], self.n_points, replace=False)]
         samples = torch.from_numpy(point_sdf_pairs).to(self.device).to(torch.float32)
         return samples, torch.full((samples.shape[0],1), idx, device=self.device)
 
